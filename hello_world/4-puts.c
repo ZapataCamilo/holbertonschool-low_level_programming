f02401e51665cae3538e03f91343913c1fd01c07
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-09 12:03:23 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- *  main - Prints "Programming is like building a multilingual
- *                   puzzle, followed by a new line.
+ * main - Entry point
  *    
- *  Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
