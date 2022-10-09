249420b3f5f166930a14190b3acb75e6d4478452
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-09 11:58:41 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,11 +2,10 @@
 
 /**
  *  main - Prints "Programming is like building a multilingual
- *                  puzzle, followed by a new line.
- *   
- *   Return: Always 0 (Success)
+ *                   puzzle, followed by a new line.
+ *    
+ *  Return: Always 0.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
