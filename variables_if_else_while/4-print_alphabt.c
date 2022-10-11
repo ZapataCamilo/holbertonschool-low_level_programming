fc0942b3524b77bc73d2e319881529256bc1f3af
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-10 18:11:15 -0700

    alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Main;
+	char Mayus;
+
+	for (Main = 'a'; Main <= 'd'; Main++)
+	{
+		putchar(Main);
+
+	}
+	for (Main = 'f'; Main <= 'p'; Main++)
+	{
+		putchar(Main);
+	}
+	for (Main = 'r'; Main <= 'z'; Main++)
+	{
+		putchar(Main);
+		if (Main == 'z')
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
