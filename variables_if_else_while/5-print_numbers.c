837c2b101ec6b14ab157f23f9f0e967bdff5d75d
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-10 18:59:50 -0700

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+	for (number == 0 ; number <= 10; number++)
+	{
+		printf("%d", number);
+		if (number == 10)
+		{
+			printf("\n");
+		}
+	}
+}
