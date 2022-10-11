fc42efacc17e54deeee372f5c7938540cafda54e
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-11 08:25:03 -0700

    print

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		
+		if (alph == 'a')
+		{
+			for (alph = 'z'; alph >= 'a'; alph--)
+			{
+				putchar(alph);
+				if (alph == 'a')
+				{
+					putchar('\n');
+				}
+				if (alph == 'a')
+				{
+					break;
+				}
+			}
+		}
+	}
+	return (0);
+}
