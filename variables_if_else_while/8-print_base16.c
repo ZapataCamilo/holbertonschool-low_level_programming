9b33c50d387c24933c1d35448d11bc4d11057697
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-11 09:08:13 -0700

    base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 char letter;
+	 int number;
+
+	 for (number = '0'; number <= '9'; number++)
+	 {
+		 putchar(number);
+		/* if (number = '9')
+		 {
+			 for (letter = 'a'; letter <= 'f'; letter++)
+			 {
+				 putchar(letter);
+				 if (letter == 'f')
+				 {
+					 putchar('\n');
+				 }
+			 }
+		 }*/
+	 }
+	 if (number = '9')
+	 {
+		 for (letter = 'a'; letter <= 'f'; letter++)
+		 {
+			 putchar(letter);
+			 if (letter == 'f')
+			 {
+				 putchar('\n');
+			 }
+		 }
+	 }
+	return (0);
+}
