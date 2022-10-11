814745000796f6ee432af240a135d6398ce512b0
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-11 12:07:09 -0700

    comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int N;
+
+	for (N = '0'; N <= '9'; N++)
+	{
+		putchar(N);
+		if (N != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	
+	}	
+	putchar('\n');
+
+	return (0);
+}
