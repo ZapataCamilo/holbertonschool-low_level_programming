63b600b0f7c005ce37640abb34e29312590eee7e
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-12 16:49:05 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/* more headers goes there */
+
+/**
+* int _islower - check the code
+*
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+	char h;
+
+	if (islower(h))
+	{
+		_putchar(h);
+	}
+	if (h = 'c' && islower(h))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
