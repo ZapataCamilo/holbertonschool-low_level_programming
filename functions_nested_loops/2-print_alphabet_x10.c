c598c585214f17a956d299a712ac957586d17f51
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-12 16:08:18 -0700

    abc10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+* print_alphabet_x10 - check the code
+*
+* Return: Always 0 (Success)
+*/
+void  print_alphabet_x10(void)
+{
+	char b;
+	int n;
+
+	for (n = '0'; n <= '10'; n++)
+	{
+		_putchar('\n');
+		for (b = 'a'; a <= 'z'; a++)
+		{
+			_putchar(b);
+		} 
+	}
+}
