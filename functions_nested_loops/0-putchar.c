6aad8838d343f285890be3dda065e64f23a1381f
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-12 10:51:02 -0700

    p

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/* more headers goes there */
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("_putchar\n");
+	char *w = "_putchar";
+
+	while (*w)
+	{
+		*w++;
+		_putchar(*w);
+	}
 
 	return (0);
 }
