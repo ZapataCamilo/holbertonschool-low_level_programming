901c0127dc9c86049c5841706c23737bcb7aed98
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-12 18:45:48 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/* more headers goes there */
+
+/**
+*print_sign - check the code
+*@n: Input char
+* Description:  The script pritn alpha case
+*
+* Return: Always 0 (Success)
+*/
+int _abs(int)
+{
+	int k;
+
+	if (k)
+	{
+		int k = abs(int);
+	}
+}
