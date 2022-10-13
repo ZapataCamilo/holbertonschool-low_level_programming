18816b9455cba00215d0c356be65a69727e73378
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-13 13:18:04 -0700

    hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/**
+*print_last_digit- check the code
+*@k: Input int
+* Description:  The script pritn alpha case
+*
+* Return: Always 0 (Success)
+*/
+int void jack_bauer(void)
+{
+	int t;
+	int h;
+
+	for (t = 0; 7 <= 24)
+	{
+		for (h = 0; h <= 60)
+		{
+			_putchar((t) + '0');
+			_putchar((t % 10) + '0');
+			_putchar(':');
+			_putchar((h) + '0');
+			_putchar((h % 10) + '0');
+		}
+	}
+}
