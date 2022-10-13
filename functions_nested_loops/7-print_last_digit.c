3a8e4139fce73546be76e884aa99fea2823c1f39
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-13 07:41:58 -0700

    las-digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/**
+*_abs- check the code
+*@k: Input int
+* Description:  The script pritn alpha case
+*
+* Return: Always 0 (Success)
+*/
+int print_last_digit(int k)
+{
+	int j = abs(k);
+
+	if (j >= 0)
+	{
+		_putchr(j, j  % 10);
+	}
+	return (0);
+}
