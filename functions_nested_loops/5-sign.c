5dd4cea9d0e914fcc1cff41452668fb51876c107
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-12 18:32:07 -0700

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/* more headers goes there */
+
+/**
+*_isalpha - check the code
+*@c: Input char
+* Description:  The script pritn alpha case
+*
+* Return: Always 0 (Success)
+*/
+int print_sign(int n)
+{
+
+	if (p > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (p == 0)
+	{
+		_putchar(0);
+		return (1);
+	}
+	else 
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+}
