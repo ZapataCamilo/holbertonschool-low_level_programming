f150eb36cf5d0ec6106f142f949ce741feb85eb8
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-13 18:11:37 -0700

    time

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/**
+*jack_bauer- check the code
+*void: Input int
+* Description:  The script pritn the hour
+*
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+	  int i;
+	  int j;
+	  int k;
+
+	  for (i = 0; i <= 9; i++)
+	  {
+		for (j = 0; j <= 9; j++)
+                {
+		     k = (((i*j) / 10));
+		     if (k >= 1)
+		     {
+			_putchar(((i*j) / 10) + '0');
+		        _putchar(((i*j) % 10) + '0');
+		     } 
+		     else
+		     {
+			_putchar(((i*j) % 10) + '0');
+		     }
+		     if (j != 9)
+		     {
+			_putchar(',');
+	               /* _putchar(' ');*/
+		     }    
+	        }
+
+	     _putchar('\n');
+          }
+}
