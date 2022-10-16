c9a3bee6a5f8992da18c6b7f05631ac5e1409e4e
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-16 10:44:07 -0700

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*mul - check the code.
+*@a: variable to check
+*@b: variable to check
+* Return: Always 0.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
