3edd4455a55f3c2904c6a40c0f425edf18350624
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-16 13:38:22 -0700

    print most numbres

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_most_numbers - check the code.
+*
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
