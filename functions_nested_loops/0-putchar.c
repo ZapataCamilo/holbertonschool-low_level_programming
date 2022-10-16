c994d9a1bea834b134182b3538466ac102c20d63
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-16 13:41:56 -0700

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,13 +10,14 @@
 */
 int main(void)
 {
-	char *w = "_putchar";
-
-	while (*w)
-	{
-		_putchar(*w);
-		w++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
