e33c0daaf0dcb9fd645ae0b12346c28e9420b8db
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-17 14:54:07 -0700

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* swap_int - check the code
+*@a: is the pointer
+*@b: is the pointer
+* Return: Always 0.
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
