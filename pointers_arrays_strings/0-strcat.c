d597ac028c90ae690fa93d53d4382b9882ef2ac6
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-20 13:49:13 -0700

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
+* _strcat - The name of the funtion
+*@dest: the pointer of the funtion
+*@src : Pointer of the funttion
 * Return: Always 0.
 */
 char *_strcat(char *dest, char *src)
 {
 	int p = 0;
 	int f = 0;
-	
+
 	while (dest[p] != '\0')
 	{
 		p++;
