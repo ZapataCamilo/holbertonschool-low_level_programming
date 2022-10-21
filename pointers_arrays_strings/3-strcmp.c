dab454bbb225c9eaaa0034c28faa788500efbc2e
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-21 12:23:56 -0700

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,18 @@ int _strcmp(char *s1, char *s2)
 {
 	int p = 0;
 
-	for (; s1[p] != s2[p]; p++)
+	while (s1[p] && s2[p] != '\0')
+	{
+		p++;
+		if (s1 != s2)
+		{
+			return (*s1 - *s2);
+		}
+	}
+
+/*	for (; s1[p] != s2[p]; p++)
 	{
 		return (*s1 - *s2);
-	}
+	}*/
 	return (0);
 }
