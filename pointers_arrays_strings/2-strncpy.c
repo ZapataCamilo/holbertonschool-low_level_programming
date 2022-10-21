fe8f1542cc68694e92452dd5e4c4ad6c302efef2
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-21 09:28:29 -0700

    copie

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strncat - check the code
+*@dest: Is a string
+*@src: Is pointer
+*@n: Is a int
+* Return: Always 0.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	for (c = 0; c < n && src[c] != '\0'; c++)
+	{
+		dest[c] = src[c];
+	}
+	for (; c < n; c++)
+	{
+		dest[c] = '\0';
+	}
+	return (dest);
+}
