5483a3ce9b9e53c425199b640575639664c215f7
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-26 07:37:55 -0700

    things

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _print_rev_recursion - check the code
+*@s: Is the string to reverse
+* Return: Always 0.
+*/
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
