611b8d3bfc81daf7d09b7110bf9a09c01e570851
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-28 12:46:58 -0700

    name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *@argc: In this code do anithing
+ *@argv: Pint the name of the code
+ * Return: Always 0.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
