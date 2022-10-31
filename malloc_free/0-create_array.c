d7febed5ad20f1131801b95668a02c3519f08647
ZapatCamilo <zapatacamilo86@gmail.com>
2022-10-31 15:18:08 -0700

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+  * create_array - Is the funtion that is call
+  *@size: Is the length of malloc
+  *@c: Is the char that I print
+  *
+  *return - return the punt
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n = 0;
+	char *punt;
+
+	punt = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (punt == NULL)
+	{
+		return (NULL);
+	}
+	for (; n < size; n++)
+	{
+		punt[n] = c;
+	}
+	return (punt);
+}
