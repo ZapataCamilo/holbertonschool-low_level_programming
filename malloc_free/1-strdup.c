87c54cabd7b4099ec1d2711b5041b22c61c2225b
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-01 12:39:46 -0700

    str

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+char *_strdup(char *str)
+{
+	unsigned int count = 0;
+	char *two = NULL;
+	unsigned int p = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	two = malloc(sizeof(*str) * count);
+
+	while (p < count)
+	{
+		two[p] = str[p];
+		p++;
+	}
+
+	if (two == NULL)
+	{
+		return (NULL);
+	}
+
+	return (two);
+	free(two);
+}
