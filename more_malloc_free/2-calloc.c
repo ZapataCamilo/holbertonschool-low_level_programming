0183284e9145ab825fa6b5f3288fbfc4970d880f
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-03 09:21:09 -0700

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ *@s: is the pointer
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str = NULL;
+
+	str = malloc(sizeof(char) * size);
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+	for(nmemb = 0; nmemb < size; nmemb++)
+		str[nmemb] = str[size];
+
+	free (str);
+	return (str);
+
+}
