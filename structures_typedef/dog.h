16ea96a4e67d814fe9114fc63dbb3167af477415
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-07 08:10:20 -0800

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+/**
+  * struct dog - contains what I need to give to the code
+  *@name: give me the name of the dog
+  *@age: give me the age of the dog
+  *@owner: the name of the owner
+  *
+  *Description: This give me the arguments that I need to take
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
