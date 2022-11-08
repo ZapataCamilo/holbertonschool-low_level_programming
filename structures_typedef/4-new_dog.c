23efc2f6c3ab14dca351626cbec10f8be4d713d1
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-08 14:17:40 -0800

    task

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+
+char *_strdup(char *str)
+{
+  unsigned int count = 0;
+  char *two = NULL;
+  unsigned int p = 0;
+
+  if (str == NULL)
+    {
+      return (NULL);
+    }
+
+  while (str[count] != '\0')
+    {
+      count++;
+    }
+
+  two = malloc(sizeof(*str) * count + 1);
+
+  if (two == NULL)
+    {
+      return (NULL);
+    }
+
+  while (p < count)
+    {
+      two[p] = str[p];
+      p++;
+    }
+  return (two);
+  free(two);
+}
 /**
   *new_dog -  prints a struct dog
   *@name: Is the name of the dog
@@ -17,9 +49,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	raza->name = name;
+
+	raza->name = _strdup(name);
 	raza->age = age;
-	raza->owner = owner;
+	raza->owner = _strdup(owner);
+
 	return (raza);
 	free(raza);
 }
