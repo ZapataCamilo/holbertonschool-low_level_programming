89ce8073aa439adf4d59236c0f96d92b72e4f179
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-09 13:39:14 -0800

    pointer function

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+  *po_sub - sub of the variables
+  *@a: Firts number
+  *@b: second number
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_add - sum of two variables  * @a: ...
+ * @b: ...
+ *
+ * Return: ...
+ */
+int op_add(int a, int b)
+{
+  return (a + b);
+}
+/**
+ * op_div - Divide two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The result.
+ */
+
+int op_div(int a, int b)
+{
+  return (a / b);
+}
+/**
+ * op_mul - product for two variables 
+ * @a: var
+ * @b: var
+ *
+ * Return: ...
+ */
+int op_mul(int a, int b)
+{
+  return (a * b);
+}
+/**
+ * op_mod - the division of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+  return (a % b);
+}
