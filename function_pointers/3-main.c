621e55c9fbb03089ebafa759a48c5d03e31eb783
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-09 13:42:44 -0800

    functions pointer

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,13 +24,13 @@ int main(int argc, char *argv[])
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(100);
 	}
 
