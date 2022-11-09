4397dcf89dcd9f6f494a2698bf87944d8b0409c9
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-09 14:42:31 -0800

    functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,53 +1,56 @@
 #include "3-calc.h"
+
 /**
-  *po_sub - sub of the variables
-  *@a: Firts number
-  *@b: second number
+  * op_sub - op add
+  * @a: number
+  * @b: number
+  * Return: soult operation
   */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_add - sum of two variables  * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+* op_add - sum of two variables
+*@b: number of add
+*@a: number of add
+*
+* Return: ...
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
- * op_div - Divide two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The result.
- */
+* op_div - Divide two numbers
+* @a: The first number
+* @b: The second number
+*
+* Return: The result
+*/
 
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
- * op_mul - product for two variables 
- * @a: var
- * @b: var
- *
- * Return: ...
- */
+* op_mul - product for two variables
+* @a: var
+* @b: var
+*
+* Return: ...
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_mod - the division of two numbers
- * @a: The first number
- * @b: The second number
- *
- * Return: result
- */
+* op_mod - the division of two numbers
+* @a: The first number
+* @b: The second number
+*
+* Return: result
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
