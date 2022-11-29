4bc1f81adedcfc04eef214127266a4cc04f1ff9c
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-29 06:57:14 -0800

    task 0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #ifndef LIST_H
 #define LIST_H
 
@@ -14,11 +11,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
-
 #endif
