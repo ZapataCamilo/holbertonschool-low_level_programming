f9054b2a1717ba84832b0b627130cc6ed75c4b68
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-29 06:49:44 -0800

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,26 +3,27 @@
 /**
  * print_list - The funsion that print the list
  * @h: the head list
- * 
+ *
  * Return: return the number of the nodes
 */
 size_t print_list(const list_t *h)
 {
-    int count_l;
-    count_l = 0;
+	int count_l;
 
-    while (h != NULL)
-    {
-        if (h->str != NULL)
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
-        else
-        {
-            printf("[0] (nil)\n");
-        }
-        count_l++;
-        h = h->next;
-    }
-    return(count_l);
+	count_l = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count_l++;
+		h = h->next;
+	}
+	return (count_l);
 }
