906cbb9a5a786c977fe38bc85a3f11720aca8be2
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-01 12:30:29 -0500

    task 3

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *add_nodeint_end - funcion to add a new node at the end
+ * @head: the first node that is empy
+ * @n: the varible fo the nodes
+ * Return: return the addres of the last node
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *new_node_end;
+
+	new_node_end = malloc(sizeof(listint_t));
+	new_node_end->n = n;
+
+	if (new_node_end == NULL)
+	{
+		return (NULL);
+		free(new_node_end);
+	}
+	if (*head == NULL)
+	{
+		*head = new_node_end;
+	}
+	else
+	{
+		new_node = *head;
+		while (new_node->next != NULL)
+		{
+			new_node = new_node->next;
+		}
+		new_node->next = new_node_end;
+	}
+	return (new_node_end);
+}
