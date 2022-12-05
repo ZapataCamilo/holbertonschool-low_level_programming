342ed39608f681a5af7b3ef58fc5f0ba09e0df61
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-05 15:11:34 -0500

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,10 +1,9 @@
-#include "main.h"
-#include <stdio.h>
+#include "<stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>"
 
 /**
  * read_textfile - To read and write it to the POSIX standard output
