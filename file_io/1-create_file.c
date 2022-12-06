1b03c78acf287c22bd8713804a079cc067eec24b
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-06 12:22:36 -0500

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+* _strlen - check the code
+*@s: is the pointer
+* Return: Always 0.
+*/
 int _strlen(char *s)
 {
 	int i = 0;
@@ -8,33 +13,38 @@ int _strlen(char *s)
 		i++;
 	return (i);
 }
+
+/**
+ *create_file - to create an empty
+ * @filename: the name of the file
+ * @text_content: the content of the file
+ * Return: Returns: 1 on success
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len;
+	int fd, len;
 	char *buff;
+
 	len = _strlen(text_content);
+
 	buff = malloc(len);
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT | O_WRONLY, 6000);
+		text_content = "";
 	}
-
+	fd = open(filename, O_CREAT, 0600);
 	if (fd == -1)
-		exit (1);
-
+		exit(1);
+	write(fd, text_content, len);
+	close(fd);
 	if (filename == NULL)
 		return (-1);
-
-	wr = write(fd, text_content, len);
-
-	close(fd);
-
 	fd = open(filename, O_RDONLY);
 
 	read(fd, buff, len);
 
 	close(fd);
 
-	return (wr);
+	return (1);
 }
