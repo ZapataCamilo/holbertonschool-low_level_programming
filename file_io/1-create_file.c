453e966ac5b857f46990688a6f47a604386a850f
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-06 15:46:58 -0500

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,14 +24,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0, len = 0, wr = 0;
 
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (text_content == NULL)
-	{
 		return (1);
-	}
 	len = _strlen(text_content);
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	wr = write(fd, text_content, len);
