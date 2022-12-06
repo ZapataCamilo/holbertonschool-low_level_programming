a2e18af50ed9237aa7b22f234c2e76358027c3e6
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-06 16:26:12 -0500

    task 1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	
 
 	fd = open(filename, O_RDWR | O_APPEND);
-
+	if (text_content == NULL)
+		return (1);
 	len = _strlen(text_content);
 	
 	if (filename == NULL)
