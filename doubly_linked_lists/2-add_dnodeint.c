754ab7c4c637a1df49b5a4fbf9517bc5a6bb4b9a
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-08 11:40:51 -0500

    task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,10 +19,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	beginning->prev = NULL;
 	*head = beginning;
 
-	while (beginning)
-	{
-		beginning = beginning->next;
-	}	
-
 	return (beginning);
 }
