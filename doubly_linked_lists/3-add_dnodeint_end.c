31ca1ec89f3df44547f43c2d0599b8a4b5cba9ff
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-09 10:18:21 -0500

    task 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
  * add_dnodeint_end - function to at a new node in the end
  * @head: is the first node
  * @n: is the variable of the struct
- * @Return: return  the address of the new element
+ * Return: return  the address of the new element
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
@@ -25,11 +25,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node = *head;
 		while (new_node->next != NULL)
-		{
 			new_node = new_node->next;
-		}
 		new_end->next = NULL;
 		new_end->prev = new_node;
+		new_node->next = new_end;
 		new_end = new_node;
 	}
 	return (new_end);
