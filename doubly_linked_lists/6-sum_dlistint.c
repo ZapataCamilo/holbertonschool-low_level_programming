36d11ddf51edcc32de84ebcf92fb27c0d3113b46
ZapataCamilo <zapatacamilo86@gmail.com>
2022-12-09 11:19:31 -0500

    task 6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -9,6 +9,8 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
+	if (head == NULL)
+		return (0);
 	while (head->next != NULL)
 	{
 		sum += head->n;
@@ -16,7 +18,5 @@ int sum_dlistint(dlistint_t *head)
 	}
 	sum += head->n;
 
-	if (head == NULL)
-		return (0);
 	return (sum);
 }
