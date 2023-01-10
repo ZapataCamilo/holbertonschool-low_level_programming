e0e2f765ecbb6b2939a6e02069ec0e2781981abb
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-10 13:22:42 -0500

    linear

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/*libreries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*funtions*/
+int linear_search(int *array, size_t size, int value);
+int main(void);
+
+
+#endif
